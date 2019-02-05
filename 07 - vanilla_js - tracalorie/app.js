addb236ba49fc6b2a8bac03d11aa2f5a60a64720
Balint <korosibalint@yahoo.co.uk>
2019-02-05 09:26:45 +0100

    delete and clear items from datastructure and UI

--- 07 - vanilla_js - tracalorie/app.js
+++ 07 - vanilla_js - tracalorie/app.js
@@ -57,6 +57,32 @@ const ItemCtrl = (function(){
       return newItem;
     },
 
+    deleteItem: function(id){
+      // My solution: 
+      // data.items.forEach(function(item, index){
+      //   if(item.id === id){
+      //     data.items.splice(index,1);
+      //   }
+      // })
+
+      // Brad's solution: 
+      // Get ids
+      const ids = data.items.map(function(item){
+        return item.id;
+      });
+
+      // Get index
+      const index = ids.indexOf(id);
+
+      // Remove item from datastructure
+      data.items.splice(index, 1);
+    },
+
+    // Clear All items from data structure
+    clearAllItems: function(){
+      data.items = [];
+    },
+
     // Loop through items and add total cals
     getTotalCalories: function(){
       // Let, because it will change
@@ -131,7 +157,8 @@ const UICtrl = (function(){
     backBtn: '.back-btn',
     itemNameInput: '#item-name',
     itemCaloriesInput: '#item-calories',
-    totalCalories: '.total-calories'
+    totalCalories: '.total-calories',
+    clearBtn: '.clear-btn'
   }
 
   // PUBLIC METHODS (ACCESSIBLE FROM OUTSIDE)
@@ -192,11 +219,30 @@ const UICtrl = (function(){
 
   },
 
+  deleteListItem: function(id){
+    const itemID = `#item-${id}`;
+    const item = document.querySelector(itemID);
+    item.remove();
+
+  },
+
   clearInput: function(){
     document.querySelector(UISelectors.itemNameInput).value = '';
     document.querySelector(UISelectors.itemCaloriesInput).value = '';
     // Bring back focus to item name
     document.querySelector(UISelectors.itemNameInput).focus();
+  },
+
+  removeItems: function(){
+    let listItems = document.querySelectorAll(UISelectors.listItems);
+    
+    
+    // Turn nodelist into array
+    listItems = Array.from(listItems);
+
+    listItems.forEach(function(item){
+      item.remove();
+    })
   },
 
   // add current item to upper form to edit. 
@@ -231,7 +277,7 @@ const UICtrl = (function(){
     document.querySelector(UISelectors.deleteBtn).style.display ='none';
     document.querySelector(UISelectors.backBtn).style.display ='none';
     document.querySelector(UISelectors.addBtn).style.display ='inline';
-
+    
   },
 
 
@@ -279,7 +325,16 @@ const App = (function(ItemCtrl, UICtrl){
     // Update item event
     document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
 
+    // Back button event - clearing the fields while editing
+    document.querySelector(UISelectors.backBtn).addEventListener('click', UICtrl.clearEditState);
 
+    // Delete button event
+    document.querySelector(UISelectors.deleteBtn).addEventListener('click', itemDeleteSubmit);
+
+    // Clear All button event
+    document.querySelector(UISelectors.clearBtn).addEventListener('click', clearAllItemsClick);
+
+    
   }
 
   // Add item submit. when you click on Add meal button
@@ -360,6 +415,48 @@ const App = (function(ItemCtrl, UICtrl){
     UICtrl.clearEditState();
 
     e.preventDefault();
+  }
+
+  const itemDeleteSubmit = function(e){
+    // Get id from currentItem
+    const currentItem = ItemCtrl.getCurrentItem();
+    
+    // Delete from datastructure
+    ItemCtrl.deleteItem(currentItem.id);
+
+    // Delete from UI
+    UICtrl.deleteListItem(currentItem.id);
+
+    //updating total calories in UI. copy paste.
+     // Get total calories
+     const totalCalories = ItemCtrl.getTotalCalories();
+
+     // Show total cals in UI
+     UICtrl.showTotalCalories(totalCalories);
+ 
+     UICtrl.clearEditState();
+
+    e.preventDefault();
+  }
+
+  // Clear Items event
+  const clearAllItemsClick = function(e){
+    // Delete all items from data structure.
+    ItemCtrl.clearAllItems();
+
+    // Remove from UI
+    UICtrl.removeItems();
+
+    //updating total calories in UI. copy paste.
+     // Get total calories
+     const totalCalories = ItemCtrl.getTotalCalories();
+
+     // Show total cals in UI
+     UICtrl.showTotalCalories(totalCalories);
+
+     // Hide unordered list (horizontal line)
+     UICtrl.hideList();
+
   }
 
   // PUBLIC METHODS (ACCESSIBLE FROM OUTSIDE)
