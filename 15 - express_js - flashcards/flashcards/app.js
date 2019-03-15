b2bb1717b7e551efa6fd835428f889736412da6f
Balint <korosibalint@yahoo.co.uk>
2019-03-15 10:59:48 +0100

    module 2 done

--- 15 - express_js - flashcards/flashcards/app.js
+++ 15 - express_js - flashcards/flashcards/app.js
@@ -2,12 +2,27 @@ const express = require("express");
 
 const app = express();
 
+//const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+
+// app.set defines settings for express
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-  res.send("pina");
+  // send just renders a string on the page, we'd use res.render to render a pug view file
+  //res.send("pina");
+  res.render("index.pug");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<h1>Hello PINA</h1>");
+app.get("/cards", (req, res) => {
+  // prompt is a variable we want to pass to the card.pug template. just use h2= prompt there OR
+  res.render("card", {
+    prompt: "who is buried in garnt's tomb?"
+    //colors
+    //hint: "think whose name it is."
+  });
+  //res.locals._variable_name_i want to pass
+  //res.locals.prompt = "Who is buried in Grant's tomb?";
+  res.render("card");
 });
 
 // port number
