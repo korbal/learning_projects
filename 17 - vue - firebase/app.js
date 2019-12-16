258282aedca7b864f921c89a02ec8e8bae6e5255
Balint <korosibalint@yahoo.co.uk>
2019-12-16 16:25:46 +0100

    lesson 15 done

--- 17 - vue - firebase/app.js
+++ 17 - vue - firebase/app.js
@@ -9,7 +9,15 @@ new Vue({
     coords: {
       x: 0,
       y: 0
-    }
+    },
+    showName: true,
+    showAge: true,
+    items: ["mushroom", "star", "fasz", "pina", "lofasz"],
+    ninjas: [
+      { name: "Balint", age: 25, belt: "black" },
+      { name: "Jozsi", age: 28, belt: "white" },
+      { name: "Sanyi", age: 12, belt: "pink" }
+    ]
   },
   methods: {
     greet(time) {
@@ -24,6 +32,18 @@ new Vue({
     logCoords(e) {
       this.coords.x = e.offsetX;
       this.coords.y = e.offsetY;
+    },
+    updateName(e) {
+      this.name = e.target.value;
+    },
+    logMessage() {
+      console.log("this was logged...");
+    },
+    toggleMario() {
+      this.showName = !this.showName;
+    },
+    toggleAge() {
+      this.showAge = !this.showAge;
     }
   }
 });
