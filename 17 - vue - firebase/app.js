8ea862f20057db491b9828efcf39786d2b53c4d5
Balint <korosibalint@yahoo.co.uk>
2019-12-16 14:39:44 +0100

    lesson 10 done

--- 17 - vue - firebase/app.js
+++ 17 - vue - firebase/app.js
@@ -2,11 +2,28 @@ new Vue({
   el: "#app",
   data: {
     title: "becoming a vue ninja",
-    name: "Ryu"
+    name: "Balint",
+    url: "https://youtube.com",
+    classes: ["one", "two"],
+    wage: 20,
+    coords: {
+      x: 0,
+      y: 0
+    }
   },
   methods: {
     greet(time) {
-      return `hello there ${this.name}, this is ${time}`;
+      return `Good ${time}, ${this.name} `;
+    },
+    changeWage(amount) {
+      this.wage += amount;
+    },
+    logEvent(e) {
+      console.log(e);
+    },
+    logCoords(e) {
+      this.coords.x = e.offsetX;
+      this.coords.y = e.offsetY;
     }
   }
 });
