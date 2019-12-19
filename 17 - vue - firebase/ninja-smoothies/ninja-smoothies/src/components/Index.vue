9693562b25fed4f62aa998f350640d56541c265c
Balint <korosibalint@yahoo.co.uk>
2019-12-19 14:33:45 +0100

    lesson 46 done, delete

--- 17 - vue - firebase/ninja-smoothies/ninja-smoothies/src/components/Index.vue
+++ 17 - vue - firebase/ninja-smoothies/ninja-smoothies/src/components/Index.vue
@@ -28,9 +28,15 @@ export default {
   },
   methods: {
     deleteSmoothie(id) {
-      this.smoothies = this.smoothies.filter(smoothie => {
-        return smoothie.id != id;
-      });
+      //delete from firestore
+      db.collection("smoothies")
+        .doc(id)
+        .delete()
+        .then(() => {
+          this.smoothies = this.smoothies.filter(smoothie => {
+            return smoothie.id != id;
+          });
+        });
     }
   },
   created() {
