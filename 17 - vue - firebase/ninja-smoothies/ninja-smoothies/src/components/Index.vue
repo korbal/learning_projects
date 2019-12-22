fc454f1ef68b81b05f50eefc121ab93e52748b11
Balint <korosibalint@yahoo.co.uk>
2019-12-22 11:16:42 +0100

    edit smooothie, lesson 52 done

--- 17 - vue - firebase/ninja-smoothies/ninja-smoothies/src/components/Index.vue
+++ 17 - vue - firebase/ninja-smoothies/ninja-smoothies/src/components/Index.vue
@@ -12,6 +12,15 @@
           </li>
         </ul>
       </div>
+      <span class="btn-floating btn-large halfwaf-fab pink">
+        <router-link
+          :to="{
+            name: 'EditSmoothie',
+            params: { smoothie_slug: smoothie.slug }
+          }"
+          ><i class="material-icons edit">edit</i></router-link
+        >
+      </span>
     </div>
   </div>
 </template>
