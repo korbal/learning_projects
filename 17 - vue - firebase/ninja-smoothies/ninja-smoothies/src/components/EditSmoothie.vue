9b7d5f90cb97f3f3e75116e56e6546e6c8ea6e05
Balint <korosibalint@yahoo.co.uk>
2019-12-22 12:17:56 +0100

    lesson 54 done

--- 17 - vue - firebase/ninja-smoothies/ninja-smoothies/src/components/EditSmoothie.vue
+++ 17 - vue - firebase/ninja-smoothies/ninja-smoothies/src/components/EditSmoothie.vue
@@ -2,6 +2,39 @@
   <div v-if="smoothie" class="edit-smoothie container">
     <!--<h2>Edit a smoothie: {{ this.$route.params.smoothie_slug }}</h2> -->
     <h2>Edit {{ smoothie.title }} Smoothie</h2>
+    <form @submit.prevent="editSmoothie">
+      <div class="field title">
+        <label for="title">Smoothie Title:</label>
+        <input type="text" name="title" v-model="smoothie.title" />
+      </div>
+      <div
+        v-for="(ing, index) in smoothie.ingredients"
+        :key="index"
+        class="field"
+      >
+        <label for="ingredient"> Ingredient: </label>
+        <input
+          type="text"
+          name="ingredient"
+          v-model="smoothie.ingredients[index]"
+        />
+        <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add an ingredient:</label>
+        <input
+          type="text"
+          name="add-ingredient"
+          @keydown.tab.prevent="addIng"
+          v-model="another"
+        />
+      </div>
+      <div class="field center-align">
+        <!--if feedback property is not null, it will show-->
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
+        <button class="btn pink">Update Smoothie</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -11,8 +44,35 @@ export default {
   name: "EditSmoothie",
   data() {
     return {
-      smoothie: null
+      smoothie: null,
+      //add another ingredient
+      another: null,
+      //if error, feedback is set to something
+      feedback: null
     };
+  },
+  methods: {
+    editSmoothie() {
+      console.log(this.smoothie.title, this.smoothie.ingredients);
+    },
+
+    addIng() {
+      if (this.another) {
+        this.smoothie.ingredients.push(this.another);
+        this.another = null;
+        this.feedback = null;
+        //if input field is empty (null) and presses tab this runs
+      } else {
+        this.feedback = "You must enter a value to add an ingredient";
+      }
+    },
+    deleteIng(ing) {
+      this.smoothie.ingredients = this.smoothie.ingredients.filter(
+        ingredient => {
+          return ingredient != ing;
+        }
+      );
+    }
   },
   created() {
     let ref = db
@@ -31,4 +91,26 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.edit-smoothie {
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500;
+}
+.edit-smoothie h2 {
+  font-size: 2em;
+  margin: 20px auto;
+}
+.edit-smoothie .field {
+  margin: 20px auto;
+  position: relative;
+}
+.edit-smoothie .delete {
+  position: absolute;
+  right: 0;
+  bottom: 16px;
+  color: #aaa;
+  font-size: 1.4em;
+  cursor: pointer;
+}
+</style>
