99d7e167247e793dd2ea210540e051e0e03db829
Balint <korosibalint@yahoo.co.uk>
2019-12-22 11:29:41 +0100

    lesson 53 done

--- 17 - vue - firebase/ninja-smoothies/ninja-smoothies/src/components/EditSmoothie.vue
+++ 17 - vue - firebase/ninja-smoothies/ninja-smoothies/src/components/EditSmoothie.vue
@@ -1,14 +1,32 @@
 <template>
-  <div class="edit-smoothie container">
-    <h2>Edit a smoothie: {{ this.$route.params.smoothie_slug }}</h2>
+  <div v-if="smoothie" class="edit-smoothie container">
+    <!--<h2>Edit a smoothie: {{ this.$route.params.smoothie_slug }}</h2> -->
+    <h2>Edit {{ smoothie.title }} Smoothie</h2>
   </div>
 </template>
 
 <script>
+import db from "@/firebase/init.js";
 export default {
   name: "EditSmoothie",
   data() {
-    return {};
+    return {
+      smoothie: null
+    };
+  },
+  created() {
+    let ref = db
+      .collection("smoothies")
+      .where("slug", "==", this.$route.params.smoothie_slug);
+    ref.get().then(snapshot => {
+      snapshot.forEach(doc => {
+        //console.log(doc.data());
+        //succesfully retrieved data from firebase, now add it to a component data
+        this.smoothie = doc.data();
+        //adding an id, which is a firebase doc id (link string)
+        this.smoothie.id = doc.id;
+      });
+    });
   }
 };
 </script>
